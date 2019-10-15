fd18d3b81547ded8c810e64a8d9777a02738fe28
Duy-Nguyen04 <55438165+Duy-Nguyen04@users.noreply.github.com>
2019-10-15 15:40:31 -0700

    Add files via upload

---
+++ Personal Projects/Duy/armeets_school_project.py
@@ -0,0 +1,16 @@
+import time
+
+name = input("Enter Name ")
+print("Hello ", name)
+age = input("Enter age ")
+print("oh your ", age)
+school = input("where do you go to school ")
+print("oh you go to", school)
+for x in range(101):
+    print("collecting location data in", x)
+print("\n collecting social security data ...")
+time.sleep(3)
+print("\n social security data collected")
+time.sleep(2)
+for x in range(1000000):
+    print("\nsexual predator here")
